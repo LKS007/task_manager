a62a181a7d4de3252638492edf1a93980c96f7b4
LKS007 <leon99_21@mail.ru>
2019-01-09 16:59:51 +0400

    Add fix to coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,10 @@
 require 'simplecov'
-SimpleCov.start
 require 'coveralls'
-Coveralls.wear!('rails')
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'app/secrets'
+end
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
