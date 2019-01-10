605869338ffe9e9e78a4d0bb30fd079d42652181
LKS007 <leon99_21@mail.ru>
2019-01-10 15:56:02 +0000

    Fix for tests

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -6,17 +6,17 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
     sign_in_as admin
   end
   test "should post create" do
-    user = attributes_for(:user)
+    user = attributes_for(:admin)
     post admin_users_url, params: { user: user }
     assert_response :redirect
   end
   test "should delete destroy" do
-    user = create(:user)
+    user = create(:admin)
     delete admin_user_url user.id
     assert_response :redirect
   end
   test "should get edit" do
-    user = create(:user)
+    user = create(:admin)
     get edit_admin_user_url user.id
     assert_response :success
   end
@@ -29,13 +29,13 @@ class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   test "should get show" do
-    user = create(:user)
+    user = create(:admin)
     get admin_users_url
     assert_response :success
   end
   test "should patch update" do
-    user = create(:user)
-    user_attrs = attributes_for(:user)
+    user = create(:admin)
+    user_attrs = attributes_for(:admin)
     patch admin_user_url user.id, params: { user: user_attrs }
     assert_response :redirect
   end
--- test/controllers/web/board_controller_test.rb
+++ test/controllers/web/board_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class Web::BoardControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
+    admin = create(:admin)
+    sign_in_as admin
     get board_url
     assert_response :success
   end
