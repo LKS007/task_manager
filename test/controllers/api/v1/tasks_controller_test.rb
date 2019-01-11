c39ff751f0479cdbf94882b31c5a1d15be297ed8
LKS007 <leon99_21@mail.ru>
2019-01-12 02:48:25 +0400

    change tests fot tasks_controller

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -1,29 +1,58 @@
 require 'test_helper'
+include Concerns::AuthHelper
 
-class Api::V1::TasksControllerTest < ActionDispatch::IntegrationTest
-  test "should get index," do
-    get api_v1_tasks_index,_url
+class Api::V1::TasksControllerTest < ActionController::TestCase
+
+  test "should get show" do
+    author = create :user
+    task = create :task, author: author
+    get :show, params: { id: task.id, format: :json }
     assert_response :success
   end
 
-  test "should get show," do
-    get api_v1_tasks_show,_url
+  test "should get index" do
+    get :index, params: { format: :json }
     assert_response :success
   end
 
-  test "should get create," do
-    get api_v1_tasks_create,_url
-    assert_response :success
+  test 'should post create' do
+    author = create :user
+    sign_in(author)
+    assignee = create :user
+    task_attributes = attributes_for(:task)
+      .merge({ assignee_id: assignee.id })
+    post :create, params: { task: task_attributes, format: :json }
+    assert_response :created
+
+    data = JSON.parse(response.body)
+    created_task = Task.find(data['id'])
+
+    assert created_task.present?
+    assert_equal task_attributes.stringify_keys, created_task.slice(*task_attributes.keys)
   end
 
-  test "should get update," do
-    get api_v1_tasks_update,_url
+  test 'should put update' do
+    author = create :user
+    assignee = create :user
+    task = create :task, author: author
+    task_attributes = attributes_for(:task)
+      .merge({ author_id: author.id, assignee_id: assignee.id })
+      .stringify_keys
+
+    patch :update, params: { id: task.id, format: :json, task: task_attributes }
     assert_response :success
+
+    task.reload
+    assert_equal task.slice(*task_attributes.keys), task_attributes
   end
 
-  test "should get destroy" do
-    get api_v1_tasks_destroy_url
+  test 'should delete destroy' do
+    author = create :user
+    task = create :task, author: author
+    delete :destroy, params: { id: task.id, format: :json }
     assert_response :success
+
+    assert !Task.where(id: task.id).exists?
   end
 
 end
