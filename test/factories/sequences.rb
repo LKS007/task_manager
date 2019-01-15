56c4780d6a9df116b2c562c7675ffe2e825b5151
LKS007 <leon99_21@mail.ru>
2019-01-16 00:00:56 +0400

    make refactoring

--- test/factories/sequences.rb
+++ test/factories/sequences.rb
@@ -5,4 +5,10 @@ FactoryBot.define do
   sequence :string do |n|
     "string#{n}"
   end
+  sequence :task_name do |n|
+    "task_name_#{n}"
+  end
+  sequence :description do |n|
+    "short description #{n}"
+  end
 end
